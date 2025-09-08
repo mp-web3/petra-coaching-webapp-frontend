92c84d54ce04c0d206267481c5d948b22373e372
Mattia <mp.web3@gmail.com>
2025-09-08 19:26:06 +0200

    feat: setted up basic navigation

---
+++ src/pages/CoachingDonna.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CoachingDonna = () => {
+    return <h1>Coaching Donna Online</h1>
+}
+
+export default CoachingDonna;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Coaching Online Personalizzato</h1>
+            <Button>scopri i piani</Button>
+        </div>
+    )
+}
+
+export default Home;
